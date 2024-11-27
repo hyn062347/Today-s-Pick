3550e487e25bded15929bdbecc5e6b47acabf3d8
bbaginuk <pbh110708@gmail.com>
2024-11-27 16:23:04 +0900

    Update MyPage.svelte

--- front-end/src/pages/MyPage.svelte
+++ front-end/src/pages/MyPage.svelte
@@ -5,9 +5,26 @@
         password: '123',    // 비밀번호
         email: 'rang@gmail.com',      // 이메일
         uname: '고해랑',       // 닉네임
-        favorite1: '',      // 선호음식1
-        favorite2: '',      // 선호음식2
+        preference: []      // 선호 음식 (체크리스트)
     };
+
+    const Options = ["곡류, 서류 제품", "과일류", "구이류",
+                     "국 및 탕류", "김치류", "나물·숙채류",
+                     "두류, 견과 및 종실류", "면 및 만두류",
+                     "밥류", "볶음류", "빵 및 과자류",
+                     "생채·무침류", "수·조·어·육류", "유제품류 및 빙과류",
+                     "음료 및 차류", "장류, 양념류", "장아찌·절임류",
+                     "전·적 및 부침류", "젓갈류", "조림류", "죽 및 스프류",
+                     "찌개 및 전골류", "찜류", "튀김류"
+                    ];
+
+    function checkOption(option) {
+        if (userData.preference.includes(option)) {
+            userData.preference = userData.preference.filter((item) => item !== option);
+        } else {
+            userData.preference = [...userData.preference, option];
+        }
+    }
 </script>
 
 <main class="setCenter">
@@ -27,13 +44,26 @@
                 <input type="ID" class="input" bind:value={userData.uid} placeholder="ID" />
                 <input type="password" class="input" bind:value={userData.password} placeholder="Password" />
                 <input type="password" class="input" placeholder="Password Confirm"/>
-                <input type="email" class="input" bind:value={userData.email} placeholder="Email"/>
-                <input type="text" class="input last" bind:value={userData.favorite1} placeholder="Favorite1"/>
-                <input type="text" class="input last" bind:value={userData.favorite2} placeholder="Favorite2"/>
+                <input type="email" class="input last" bind:value={userData.email} placeholder="Email"/>
+                <fieldset>
+                    <legend>선호 음식 종류</legend>
+                    <div class="preferences">
+                        {#each Options as option}
+                            <label class="checkbox">
+                                <span 
+                                    class:checked={userData.preference.includes(option)}
+                                    on:click={() => checkOption(option)}
+                                >
+                                    {option}
+                                </span>
+                            </label>
+                        {/each}
+                    </div>
+                </fieldset>
             </div>
             <div>
-                <button type="reset">Cancel</button>
-                <button type="submit">Confirm</button>
+                <button class="cancel" type="reset">Cancel</button>
+                <button class="confirm" type="submit">Confirm</button>
             </div>
         </form>
     </div>
@@ -57,8 +87,9 @@
 
     .form-box {
         position: absolute;
-        top: 15%;
-        max-width: 350px;
+        top: 10%;
+        width: 75%;
+        max-width: 600px;
         background: #f1f7fe;
         overflow: hidden;
         border-radius: 16px;
@@ -84,11 +115,6 @@
         margin-bottom: 10px;
     }
 
-    .subtitle {
-        font-size: 1rem;
-        color: #666;
-    }
-
     /*Inputs box*/
     .form-container {
         overflow: hidden;
@@ -100,6 +126,7 @@
     }
 
     .input {
+        font-size: 18px;
         background: none;
         border: 0;
         outline: 0;
@@ -107,10 +134,12 @@
         width: 100%;
         border-bottom: 1px solid #eee;
         padding: 8px 15px;
+        margin-bottom: 2px;
     }
 
     .name{
         font-size: 1.4rem;
+        font-weight: 500;
         display: flex;
         text-align: center;
         margin-bottom: 0px;
@@ -119,27 +148,47 @@
     .last{
         margin-bottom: 0px;
     }
-    .form-section {
-        padding: 16px;
-        font-size: 0.85rem;
-        background-color: #e0ecfb;
-        box-shadow: rgb(0 0 0 / 8%) 0 -1px;
+
+    /* fieldset */
+    fieldset {
+        border: none;
+        padding: 0;
+        margin: 1rem 0;
     }
 
-    .form-section a {
+    fieldset legend {
+        font-size: 16px;
         font-weight: bold;
-        color: #0066ff;
-        transition: color 0.3s ease;
+        margin-bottom: 20px;
     }
 
-    .form-section a:hover {
-        color: #005ce6;
-        text-decoration: underline;
+    .preferences {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 12px;
+    }
+
+    .preferences span {
+        padding: 5px 10px;
+        background-color: #f0f0f0;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease, color 0.3s ease;
+    }
+
+    .preferences span:hover {
+        background-color: #0055d4;
+        color: #f0f0f0;
+    }
+
+    .preferences span.checked {
+        background-color: #0066ff;
+        color: #f0f0f0;
     }
 
     /*Button*/
     .form button {
-        background-color: #0066ff;
         color: #fff;
         border: 0;
         border-radius: 15px;
@@ -150,7 +199,19 @@
         transition: background-color 0.3s ease;
     }
 
-    .form button:hover {
-        background-color: #005ce6;
+    .confirm {
+        background-color: #0066ff;
+    }
+
+    .form .confirm:hover {
+        background-color: #0055d4;
+    }
+
+    .cancel {
+        background-color: #c9c9c9;
+    }
+
+    .form .cancel:hover {
+        background-color: #a0a0a0;
     }
 </style>
