57d1b33cede23b5a5f9422d361098b641516790a
bbaginuk <pbh110708@gmail.com>
2024-11-27 16:27:27 +0900

    noImg

--- myspring/src/main/java/com/dita/myapp/repository/FavoriteRepository.java
+++ myspring/src/main/java/com/dita/myapp/repository/FavoriteRepository.java
@@ -14,7 +14,7 @@ public interface FavoriteRepository extends CrudRepository<Favorite, FavoriteId>
     @Query(value = "select count(*) from Favorite where uid=?1 and mid=?2",nativeQuery = true)
     public void findByFavorite(String uid,int mid);
 
-    @Query(value = "SELECT favorite.rid, ifnull(r.rimg_src,'null'), ifnull(r.rimg_name,'null'), r.recipe_title, r.mname, r.ctg, favorite.added_at FROM favorite JOIN (SELECT recipe.rid, recipe.rimg_src, recipe.rimg_name, recipe.recipe_title, menu.mname, menu.ctg FROM recipe JOIN menu ON recipe.mid=menu.mid) r ON favorite.rid=r.rid WHERE uid=?1",nativeQuery = true)
+    @Query(value = "SELECT favorite.rid, ifnull(r.rimg_src,'/img/'), ifnull(r.rimg_name,'noImg.png'), r.recipe_title, r.mname, r.ctg, favorite.added_at FROM favorite JOIN (SELECT recipe.rid, recipe.rimg_src, recipe.rimg_name, recipe.recipe_title, menu.mname, menu.ctg FROM recipe JOIN menu ON recipe.mid=menu.mid) r ON favorite.rid=r.rid WHERE uid=?1",nativeQuery = true)
     public List<Object[]> findByUidToFavorite(String uid);
     
 
