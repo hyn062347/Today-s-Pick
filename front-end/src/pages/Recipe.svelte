955cb69f9d022f3424a3b51c9f4a628ccf55d742
bbaginuk <pbh110708@gmail.com>
2024-12-12 15:34:14 +0900

    Update Recipe.svelte

--- front-end/src/pages/Recipe.svelte
+++ front-end/src/pages/Recipe.svelte
@@ -2,24 +2,45 @@
     import { navigate } from "svelte-routing";
 
     let image = "/img/SequoiaLight.png";
-    function editData() {
-        console.log("Edit");
-    }
-    function deleteData() {
-        console.log("Delete");
-    }
+
+    let recipes = [
+        {
+            image: '/img/noImg.png',        // 이미지 파일
+            menu: '메뉴이름',           // 메뉴 이름
+            title: '레시피 제목1',          // 레시피 제목
+            category: '카테고리1',       // 분류
+            uid: 'GPT'      // 작성자 UID (임시 값)
+        },
+        {
+            image: '/img/noImg.png',        // 이미지 파일
+            menu: '메뉴이름',           // 메뉴 이름
+            title: '레시피 제목2',          // 레시피 제목
+            category: '카테고리2',       // 분류
+            uid: 'GPT'      // 작성자 UID (임시 값)
+        },
+        {
+            image: '/img/noImg.png',        // 이미지 파일
+            menu: '메뉴이름',           // 메뉴 이름
+            title: '레시피 제목3',          // 레시피 제목
+            category: '카테고리3',       // 분류
+            uid: 'GPT'      // 작성자 UID (임시 값)
+        },
+    ]
+
 </script>
 
 <main class="main">
     <h1>Recipe</h1>
-
-    <div class="element" on:click={() => navigate('/recipedetail')}>
-        <img src={image} alt="Image" />
-        <div class="stringBox">
-            <span class="title">Title</span>
-            <span class="subtitle">Subtitle</span>
+    {#each recipes as recipe (recipe.title)}
+        <div class="element" on:click={() => navigate('/recipedetail')}>
+            <img src={recipe.image} alt="Image" />
+            <div class="stringBox">
+                <span class="title">{recipe.title}</span>
+                <span class="subtitle">{recipe.menu}, {recipe.category}</span>
+                <span class="subtitle">작성자 - {recipe.uid}</span>
+            </div>
         </div>
-    </div>
+    {/each}
     <!-- --------------^형식으로 만들면 됨--------------- -->
 
     <!-- --------------테스트용--------------- -->
@@ -73,6 +94,7 @@
     }
 
     .stringBox .subtitle {
+        color: #a9a9a9;
         font-size: 16px;
         font-family: 'Inter';
     }
